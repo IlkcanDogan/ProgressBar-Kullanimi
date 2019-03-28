2cfe76a77c9db6a492e1beaa0ffc4fee8866126a
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-29 00:20:40 +0300

    Add files via upload

---
+++ Uygulama Yükleyici/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDevam = new System.Windows.Forms.Button();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.pBDurum = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDevam
+            // 
+            this.btnDevam.Location = new System.Drawing.Point(340, 161);
+            this.btnDevam.Name = "btnDevam";
+            this.btnDevam.Size = new System.Drawing.Size(112, 23);
+            this.btnDevam.TabIndex = 0;
+            this.btnDevam.Text = "Devam >>";
+            this.btnDevam.UseVisualStyleBackColor = true;
+            this.btnDevam.Click += new System.EventHandler(this.btnDevam_Click);
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAd.Location = new System.Drawing.Point(44, 37);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(399, 55);
+            this.lblAd.TabIndex = 1;
+            this.lblAd.Text = "İLKCAN DOĞAN";
+            // 
+            // pBDurum
+            // 
+            this.pBDurum.Location = new System.Drawing.Point(21, 115);
+            this.pBDurum.Name = "pBDurum";
+            this.pBDurum.Size = new System.Drawing.Size(431, 23);
+            this.pBDurum.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(20, 142);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(16, 13);
+            this.lblDurum.TabIndex = 3;
+            this.lblDurum.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 196);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.pBDurum);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.btnDevam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDevam;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.ProgressBar pBDurum;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
+
---
+++ Uygulama Yükleyici/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDevam_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDevam.Enabled = false;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int artişOrani = rnd.Next(1,20);
+            pBDurum.Step = artişOrani;
+            pBDurum.PerformStep();
+
+            int suAnkiDeger = pBDurum.Value;
+
+            if (suAnkiDeger <= 20)
+            {
+                lblDurum.Text = "Program yüklemeye hazırlanıyor...";
+            }
+            else if (suAnkiDeger > 20 && suAnkiDeger <= 45)
+            {
+                lblDurum.Text = "Güvenlik modülü yükleniyor...";
+            }
+            else if (suAnkiDeger > 45 && suAnkiDeger <= 70)
+            {
+                lblDurum.Text = "Veritabanı modülü yükleniyor...";
+            }
+            else if (suAnkiDeger > 70 && suAnkiDeger <= 95)
+            {
+                lblDurum.Text = "Bellek optimizasyonu yapılıyor...";
+            }
+            else if (suAnkiDeger > 95 && suAnkiDeger < 100)
+            {
+                lblDurum.Text = "Yükleme tamamlanıyor...";
+            }
+            else {
+                
+                timer1.Stop();
+                lblDurum.Text = "Yükleme başarı ile tamamlandı!";
+                btnDevam.Enabled = true;
+            }
+
+        }
+    }
+}
